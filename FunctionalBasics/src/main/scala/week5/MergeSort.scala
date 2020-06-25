db8d0edc54f640270e59f669f8e0d9d304184edb
Navi <naval.kumar99@gmail.com>
2020-06-25 19:26:23 +0800

    implicit and generics applied on merge sort

--- FunctionalBasics/src/main/scala/week5/MergeSort.scala
+++ FunctionalBasics/src/main/scala/week5/MergeSort.scala
@@ -2,7 +2,7 @@ package week5
 
 object MergeSort {
 
-  def msort(xs: List[Int]): List[Int]  ={
+  def msort[T](xs: List[T])(implicit c : (T,T) => Boolean): List[T]  ={
     val n = xs.length / 2
     if(n == 0) xs
     else {
@@ -11,16 +11,19 @@ object MergeSort {
     }
   }
 
-  private def merge(xs : List[Int], ys: List[Int]): List[Int] = {
+  private def merge[T](xs : List[T], ys: List[T])(implicit c : (T,T) => Boolean): List[T] = {
     (xs , ys) match {
       case (l ,List()) => l
       case (List(), r) => r
-      case (l:: ls , r :: rs) => if(l < r) l :: merge(ls, ys) else r :: merge(xs , rs)
+      case (l:: ls , r :: rs) => if(c(l,r)) l :: merge(ls, ys) else r :: merge(xs , rs)
     }
   }
 
   def main(args: Array[String]): Unit = {
+    implicit def intCompare(x: Int, y: Int) : Boolean = x < y
+    implicit def stringCompare(x:String, y:String) : Boolean = x.compareTo(y) < 0
     println(merge(1 :: 3 :: List(5), 2 :: 4 :: List(6)))
     println(msort( 2314 :: 5616 :: 6656 :: 1 :: 42516 :: 67 :: 567 :: List(0)))
+    println(msort(List("abc", "aab", "aba")))
   }
 }
