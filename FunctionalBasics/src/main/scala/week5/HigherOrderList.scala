9a487d718af0fe97b17babbc4314943d522eb62b
Navi <naval.kumar99@gmail.com>
2020-06-28 12:07:13 +0800

    completed week5

--- FunctionalBasics/src/main/scala/week5/HigherOrderList.scala
+++ FunctionalBasics/src/main/scala/week5/HigherOrderList.scala
@@ -19,10 +19,44 @@ object HigherOrderList {
 
   def squareListSimple(xs : List[Int]) : List[Int] = xs map (x => x * x)
 
+  def pack[T](xs:List[T]): List[List[T]] = xs match {
+    case Nil => Nil
+    case y :: ys =>
+      val (first, rest) = xs span (z => y == z)
+      first :: pack(rest)
+  }
+
+  def encode[T](xs: List[T]): List[(T,Int)] =
+    pack(xs) map (y => (y.head, y.length))
+
+  @scala.annotation.tailrec
+  def foldLeft[U, T](x: List[T], z: U)(op : (U, T) => U) : U = {
+    x match {
+      case Nil => z
+      case y :: ys => foldLeft(ys, op(z, y))(op)
+    }
+  }
+
+  def foldRight[U, T](x: List[T], z: U)(op : (T, U) => U) : U = {
+    x match {
+      case Nil => z
+      case y :: ys => op(y, foldRight(ys, z)(op))
+    }
+  }
+
   def main(args: Array[String]): Unit = {
     val l = 1 :: 2 :: -3 :: List(4)
     println(squareList(l))
     println(squareListSimple(l))
     println(posInts(l))
+
+    println(pack(1 :: 2 :: 2 :: 3 :: 3 :: 3 :: List(1))) //List(List(1), List(2, 2), List(3, 3, 3), List(1))
+    println(encode(1 :: 2 :: 2 :: 3 :: 3 :: 3 :: List(1))) //List((1,1), (2,2), (3,3), (1,1))
+
+    println(foldLeft(l,0)(_ + _))
+    println(foldRight(l,0)(_ + _))
+
+    println(foldRight(l, 100 :: -10 :: Nil) (_ :: _))
+    //println(foldLeft(l, 100 :: Nil) (_ :: _)) compile error
   }
 }
--- FunctionalBasics/src/main/scala/week5/listfun.sc
+++ FunctionalBasics/src/main/scala/week5/listfun.sc
@@ -8,3 +8,14 @@ nums partition( r => r > 0)
 nums takeWhile(x => x > 0)
 nums dropWhile( x => x < 0)
 nums span ( x => x < 0)
+
+
+def mapFun[T, U](xs: List[T], f: T => U): List[U] =
+  (xs foldRight List[U]())( (t, u) => f(t) :: u)
+
+def lengthFun[T](xs: List[T]): Int =
+  (xs foldRight 0)( (_, u) => u + 1 )
+
+lengthFun(1 :: 2 :: 3 :: 1 :: 1 :: 1::1::1::1 ::Nil)
+mapFun(1 :: 2 :: 3 :: Nil, (x:Int) => x + x )
+
