1c66c41194a7b423e5022a9708ea4ccca2f4612e
Navi <naval.kumar99@gmail.com>
2020-08-09 21:56:36 +0800

    completed week 2 assignment

---
+++ parallel-computing/reductions/src/main/scala/reductions/ParallelParenthesesBalancing.scala
@@ -0,0 +1,103 @@
+package reductions
+
+import scala.annotation._
+import org.scalameter._
+
+object ParallelParenthesesBalancingRunner {
+
+  @volatile var seqResult = false
+
+  @volatile var parResult = false
+
+  val standardConfig = config(
+    Key.exec.minWarmupRuns -> 40,
+    Key.exec.maxWarmupRuns -> 80,
+    Key.exec.benchRuns -> 120,
+    Key.verbose -> true
+  ) withWarmer(new Warmer.Default)
+
+  def main(args: Array[String]): Unit = {
+    val length = 100000000
+    val chars = new Array[Char](length)
+    val threshold = 10000
+    val seqtime = standardConfig measure {
+      seqResult = ParallelParenthesesBalancing.balance(chars)
+    }
+    println(s"sequential result = $seqResult")
+    println(s"sequential balancing time: $seqtime")
+
+    val fjtime = standardConfig measure {
+      parResult = ParallelParenthesesBalancing.parBalance(chars, threshold)
+    }
+    println(s"parallel result = $parResult")
+    println(s"parallel balancing time: $fjtime")
+    println(s"speedup: ${seqtime.value / fjtime.value}")
+  }
+}
+
+object ParallelParenthesesBalancing extends ParallelParenthesesBalancingInterface {
+
+  /** Returns `true` iff the parentheses in the input `chars` are balanced.
+   */
+  def balance(chars: Array[Char]): Boolean = {
+    def balanceIter(value: List[Char], acc: Int) : Boolean = {
+      if (value.isEmpty) {
+        if(acc == 0) true else false
+      }else{
+        if(value.head == '(' ) {
+          balanceIter(value.tail, acc + 1)
+        }
+        else if (value.head == ')') {
+          if(acc > 0)
+            balanceIter(value.tail, acc - 1)
+          else
+            false
+        }
+        else balanceIter(value.tail , acc)
+      }
+    }
+    balanceIter(chars.toList, 0)
+  }
+
+  /** Returns `true` iff the parentheses in the input `chars` are balanced.
+   */
+  def parBalance(chars: Array[Char], threshold: Int): Boolean = {
+
+    def traverse(idx: Int, until: Int, arg1: Int, arg2: Int): (Int , Int) = {
+      var curr = idx
+      var l = 0
+      var r =  0
+      while(curr < until) {
+        if(chars(curr) == '(' )
+          l += 1
+        else if(chars(curr) == ')') {
+          if(l > 0) l -= 1
+          else r += 1
+        }
+        curr = curr + 1
+      }
+      (l,r)
+    }
+
+    def reduce(from: Int, until: Int): (Int,Int) = {
+      if( until - from <= threshold)
+        traverse(from, until, 0, 0)
+      else {
+        val mid = (from + until) / 2
+
+        val ((l1,r1),(l2,r2)) = parallel( reduce(from, mid), reduce(mid, until))
+
+        if(l1 + l2 >= r2)
+          (l1 + l2 - r2, r1)
+        else
+          (l1 + l2 , r1 + r2)
+      }
+    }
+
+    reduce(0, chars.length) == (0, 0)
+  }
+
+  // For those who want more:
+  // Prove that your reduction operator is associative!
+
+}
