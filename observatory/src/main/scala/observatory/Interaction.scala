d3dbd830097ed5ebd6b5fbf49b10f5b86d1179c1
Navi-nk <naval.kumar99@gmail.com>
2020-09-26 00:53:32 +0800

    completed week3 milestone

--- observatory/src/main/scala/observatory/Interaction.scala
+++ observatory/src/main/scala/observatory/Interaction.scala
@@ -1,18 +1,27 @@
 package observatory
 
 import com.sksamuel.scrimage.{Image, Pixel}
+import observatory.Visualization.{interpolateColor, predictTemperature}
+
+import scala.math.{Pi, atan, pow, sinh}
 
 /**
   * 3rd milestone: interactive visualization
   */
 object Interaction extends InteractionInterface {
-
+  val width: Int =256
+  val height: Int =256
+  val alpha: Int = 127
   /**
     * @param tile Tile coordinates
     * @return The latitude and longitude of the top-left corner of the tile, as per http://wiki.openstreetmap.org/wiki/Slippy_map_tilenames
     */
   def tileLocation(tile: Tile): Location = {
-    ???
+    val n = pow(2, tile.zoom)
+    val lon_deg = (tile.x / n) * 360.0 - 180.0
+    val lat_rad = atan(sinh(Pi * (1 - 2 * tile.y / n)))
+    val lat_deg = lat_rad * 180.0 / Pi
+    Location(lat_deg, lon_deg)
   }
 
   /**
@@ -22,7 +31,23 @@ object Interaction extends InteractionInterface {
     * @return A 256×256 image showing the contents of the given tile
     */
   def tile(temperatures: Iterable[(Location, Temperature)], colors: Iterable[(Temperature, Color)], tile: Tile): Image = {
-    ???
+    val offX = tile.x * width
+    val offY = tile.y * height
+
+    val allCords = for {
+      i <- 0 until height
+      j <- 0 until width
+    } yield (i, j)
+
+    val pixels = allCords
+      .map({case (y, x) => Tile(x + offX, y + offY, tile.zoom + 8)})
+      .map(tileLocation)
+      .map(predictTemperature(temperatures, _))
+      .map(interpolateColor(colors, _))
+      .map(col => Pixel(col.red, col.green, col.blue, alpha))
+      .toArray
+
+    Image(width, height, pixels)
   }
 
   /**
@@ -36,7 +61,12 @@ object Interaction extends InteractionInterface {
     yearlyData: Iterable[(Year, Data)],
     generateImage: (Year, Tile, Data) => Unit
   ): Unit = {
-    ???
+    for {
+      zoom <- 0 until 4
+      x <- 0 until pow(2, zoom).toInt
+      y <- 0 until pow(2, zoom).toInt
+      yearData <- yearlyData
+    } yield generateImage(yearData._1, Tile(x, y, zoom), yearData._2)
   }
 
 }
